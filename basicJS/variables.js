02617b6ae7ac3931a76b188deb8a048c6e80ad30
John Corner <54984832+johncorneron@users.noreply.github.com>
2024-07-17 09:10:42 +0000

    Folder name change

---
+++ basicJS/variables.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountMail = "arpit.12345@gmail.com"
+var accountPassword = "5555"
+accountCity = "Raj Nagar"
+let accountState;
+
+
+// accountId = 2
+accountMail = "sourabh12345@gmail.com"
+accountPassword = "12345"
+accountCity = "Delhi"
+
+// console.log(accountId);
+console.table([accountId, accountMail, accountPassword, accountCity, accountState])
