ab3993c679374f98d30aa56d21414b5a048db93d
John Corner <54984832+johncorneron@users.noreply.github.com>
2024-07-18 06:38:39 +0000

    coversionOperatores.js

---
+++ basicJS/conversionOperation.js
@@ -0,0 +1,30 @@
+let Ten = 10
+console.log(Ten);
+
+let score = "70"
+console.log(typeof score);
+console.log(typeof (score));
+
+// Convert in number
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" = 33 Number
+// "33abc" = NaN (Not a number) (But typr of NaN is number)
+// true = 1, false = 0
+
+// Convert in boolean
+let isLoggedIn = 10
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 10 = true, 0 = false
+// "" = false
+// "Arpit" = true
+
+// Convert in string
+let theNum = 100
+let stringNum = String(theNum)
+console.log(stringNum);
+console.log(typeof stringNum);
