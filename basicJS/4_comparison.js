2c61f5240f56b94eaf6ff63b81bf59a2dd1829f1
John Corner <54984832+johncorneron@users.noreply.github.com>
2024-07-19 09:35:59 +0000

    comparison.js & rename all folders name

---
+++ basicJS/4_comparison.js
@@ -0,0 +1,14 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)
+
+console.log("5">1);
+console.log("05">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null!=0);
+
+console.log("10"===10);
