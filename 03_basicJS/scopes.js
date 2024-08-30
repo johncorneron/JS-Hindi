9c2005db08ce54efe6e80141ffeccbcecfa9f16d
John Corner <54984832+johncorneron@users.noreply.github.com>
2024-08-30 06:04:36 +0000

    scopes.js

---
+++ 03_basicJS/scopes.js
@@ -0,0 +1,35 @@
+function one() {
+
+    const username = "Arpit"
+
+    function two() {
+        const website = "kayoanime"
+        console.log(username);
+
+        // console.log(website);
+    }
+
+    // two()
+}
+
+// one()
+
+if (true) {
+    const username = "Hitesh"
+    if (username === "Hitesh") {
+        const website = " YouTube"
+        console.log(username + website);
+        console.log(website);
+    }
+}
+
+function addOne(numOne) {
+    return numOne + 1
+}
+console.log(addOne(5));
+
+
+function addTwo(numTwo) {
+    return numTwo + 1
+}
+console.log(addTwo(5));
