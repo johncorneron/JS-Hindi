0b61ba8463f90070c1e5590596910d2812d1e96d
John Corner <54984832+johncorneron@users.noreply.github.com>
2024-08-30 12:00:27 +0000

    Arrow.js

--- 03_basicJS/03_arrow.js
+++ 03_basicJS/03_arrow.js
@@ -14,3 +14,20 @@ function global() {
     console.log(this);
 }
 global()
+
+const otherName = () => {
+    username: "Sourabh SUmal",
+    console.log(this);
+}
+otherName()
+
+const addNum = (addOne, addTwo) => {
+    return addOne + addTwo
+}
+console.log(addNum(50, 100));
+
+const addNumber = (addThree, addFour) => (addThree + addFour)
+console.log(addNumber(10, 40));
+
+const otherEx = (otherOne, otherTwo) => ({userName: "Bhanu"})
+console.log(otherEx(1, 2));
