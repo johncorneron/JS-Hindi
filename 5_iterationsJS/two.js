8f4b876ea46e1dec7066bac6ca26300d40a15c74
John Corner <54984832+johncorneron@users.noreply.github.com>
2024-10-23 09:16:49 +0000

    Added two.js file

---
+++ 5_iterationsJS/two.js
@@ -0,0 +1,20 @@
+// while & do while loops
+let index = 0
+while (index <= 5) {
+    console.log(`The value of index is ${index}`);
+    index = index + 1
+}
+
+let indieSupes = ["Inspector Steel", "Doga", "Shakti", "Anthony"]
+let indieNum = 0
+while (indieNum < indieSupes.length) {
+    console.log(`SuperHero - ${indieSupes[indieNum]}`);
+    indieNum = indieNum + 1
+}
+
+// do while loop
+let score = 0
+do {
+    console.log(`The score is ${score}`);
+    score++
+} while (score <= 5);
