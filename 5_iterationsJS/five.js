730f21c9c2bdfede58e10c2b55b001cd335107ae
John Corner <54984832+johncorneron@users.noreply.github.com>
2024-11-04 07:11:54 +0000

    five.js updated

--- 5_iterationsJS/five.js
+++ 5_iterationsJS/five.js
@@ -6,3 +6,26 @@ coding.forEach(function (value) {
 coding.forEach( (item) => {
     console.log(item);
 })
+
+coding.forEach( (item, index, arr) => {
+    console.log(item, index, arr);
+});
+
+const programmingLan = [
+    {
+        languageName: "JAVASCRIPT",
+        languageFileName: "js"
+    },
+    {
+        languageName: "CSS3",
+        languageFileName: "css"
+    },
+    {
+        languageName: "HTML5",
+        languageFileName: "html"
+    }
+]
+
+programmingLan.forEach( (item) => {
+    console.log(item.languageName);
+});
